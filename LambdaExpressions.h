37d1922b00fcded8f863c0eee2386590554a77b1
melingiusti <melingiusti@melingiusti-VirtualBox>
2018-08-25 16:52:13 +0200

    Master 15

---
+++ LambdaExpressions.h
@@ -0,0 +1,14 @@
+/*
+ * LambdaExpressions.h
+ *
+ *  Created on: 25 ago 2018
+ *      Author: melingiusti
+ */
+
+#ifndef LAMBDAEXPRESSIONS_H_
+#define LAMBDAEXPRESSIONS_H_
+
+
+int getNumber(int &n);
+
+#endif /* LAMBDAEXPRESSIONS_H_ */
