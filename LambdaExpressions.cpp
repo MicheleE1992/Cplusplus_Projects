0b05c589eb449fc678c32aac03cd2da85c226b83
melingiusti <melingiusti@melingiusti-VirtualBox>
2018-08-25 15:50:54 +0200

    alessio_2 12

--- LambdaExpressions.cpp
+++ LambdaExpressions.cpp
@@ -89,7 +89,7 @@ int main() {
 															   //newInt2 can't be modified as it is read-only variable after passing by value
 	cout << newInt << endl;
 	//alessio_3: Changed 11
-
+	//alessio_3: Added 12
 	[&](){ cout << "Welcome " << endl; newInt++, newInt2++; }(); //Lambda Capture Expression all local variables by reference
 	cout << newInt << " " << newInt2 << endl;
 
